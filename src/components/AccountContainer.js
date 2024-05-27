1f544c9905b80bd769b9111e530bcc26e5dba830
mercynjambi <njambim695@gmail.com>
2024-05-27 19:49:20 +0300

    updated AccountContainer

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transaction, setTransaction] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions?q=" + query)
+      .then((resp) => resp.json())
+      .then((data) => setTransaction(data))
+  }, [query]);
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+  }
+
   return (
     <div>
-      <Search />
+      <Search handleSearch={handleSearch} />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transactions={transaction} />
     </div>
   );
 }

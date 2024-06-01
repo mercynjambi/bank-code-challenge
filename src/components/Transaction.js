4439c07b45d6685b98d47c5c4b9477f2f151de89
MERCY NJAMBI <njambim695@gmail.com>
2024-06-01 12:05:26 +0300

    Updated  delete button

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function Transaction({date, description, category, amount}) {
+function Transaction({ id, date, description, category, amount, handleDelete }) {
   return (
     <tr>
       <td>{date}</td>
       <td>{description}</td>
       <td>{category}</td>
       <td>{amount}</td>
+      <td>
+        <button className="ui button" onClick={() => handleDelete(id)}>Delete</button>
+      </td>
     </tr>
   );
 }
